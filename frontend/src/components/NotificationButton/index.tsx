dbdfa4cba88896d5e785fa3f9c9923869fccc35c
Talitha Ciancio <titalita35@hotmail.com>
2022-07-17 20:08:50 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assests/img/Notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id :number){
     axios(`${BASE_URL}/sales/${id}/notification`)
       .then(response =>{
-        console.log("sucesso")
+        toast("SMS enviado com Sucesso");
       })
 }
 
